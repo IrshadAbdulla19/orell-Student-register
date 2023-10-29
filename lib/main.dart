925defcaf1120a6b62dd672088487f9d0c1448cb
IrshadAbdulla19 <irshadabdulla19@gmail.com>
2023-10-29 16:23:08 +0530

    debugbanner removed

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: SplashScreen(),
       ),
     );
